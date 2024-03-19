b1cae6f4919163120302a22e592b8e4fb6fea715
anjalidk <67758175+anjalidk@users.noreply.github.com>
2024-03-19 16:39:49 -0700

    mem.sh fix (#41)
    On the Ubuntu system on which qa_test.yml is run, gcc does not seem to
    accept -lresource as a CFLAGS. Instead, added this on the 'make' target.
    In addition, in mem.sh, we need to cd to the correct directory, before
    running make.
    
    Signed-off-by: Anjali Kulkarni <anjali.k.kulkarni@oracle.com>

--- tests/MEM/mem.sh
+++ tests/MEM/mem.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 # Enable -DTESTING in Makefile and the recompile library - make
-make clean
 
+export LD_LIBRARY_PATH=`git rev-parse --show-toplevel`
+cd $LD_LIBRARY_PATH/tests/MEM
+
+make clean
 make mem_test
+
 cat /proc/meminfo > mem_info.orig
 sed -i 's/[ ]\+/ /g' mem_info.orig
 ./mem_test
